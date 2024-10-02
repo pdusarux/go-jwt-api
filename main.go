b217558fa0aa0672e7bcfae3e37f3c3f3097553b
pdusarux <p.dusarux@gmail.com>
2024-10-02 14:11:24 +0700

    Can encrypt password and register user

--- main.go
+++ main.go
@@ -46,17 +46,37 @@ func main() {
 			})
 			return
 		}
-		encryptedPassword, err := bcrypt.GenerateFromPassword([]byte(json.Password), 10)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
+
+		// Check Username Exists
+		var userExist User
+		db.Where("username = ?", json.Username).First(&userExist)
+		if userExist.ID > 0 {
+			c.JSON(http.StatusOK, gin.H{"status": "error", "message": "User Exists"})
+			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{
-			"register":          json,
-			"encryptedPassword": encryptedPassword,
-		})
+		// Encrypt Password
+		encryptedPassword, err := bcrypt.GenerateFromPassword([]byte(json.Password), 10)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+
+		// Declare variables and insert data
+		user := User{Username: json.Username, Password: string(encryptedPassword),
+			Fullname: json.Fullname, Avatar: json.Avatar}
+
+		// Save in db
+		if result := db.Create(&user); result.Error != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+
+		// Return result
+		if user.ID > 0 {
+			c.JSON(http.StatusOK, gin.H{"status": "ok", "message": "User Registered", "userId": user.ID})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"status": "error", "message": "User Create Failed"})
+		}
+
 	})
 
 	r.Run(":8000")
