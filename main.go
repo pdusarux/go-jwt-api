a61b5ff9112ccd0bf12e5692919bea49e034fd73
pdusarux <p.dusarux@gmail.com>
2024-10-02 13:52:29 +0700

    Connect MySQL by docker-compose, AutoMigrate

--- main.go
+++ main.go
@@ -4,14 +4,48 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+type Register struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Fullname string `json:"fullname" binding:"required"`
+	Avatar   string `json:"avatar" binding:"required"`
+}
+
+type User struct {
+	gorm.Model
+	Username string
+	Password string
+	Fullname string
+	Avatar   string
+}
+
 func main() {
+	dsn := "root:root_password@tcp(127.0.0.1:3306)/mydb?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&User{})
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+
+	r.POST("/register", func(c *gin.Context) {
+		var json Register
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"register": json,
 		})
 	})
-	r.Run(":8080")
+
+	r.Run(":8000")
 }
